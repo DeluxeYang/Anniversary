deea78b1a846293ce025094b334bbcb53a97417a
idear <hui.wu@8lab.cn>
2017-05-22 18:43:29 +0800

    2017-5-22

--- Anniversary/gallery/views.py
+++ Anniversary/gallery/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from gallery.models import pic
 # Create your views here.
 def index(request):
-	pics = pic.objects.all()
+	pics = pic.objects.all().order_by("pic_time")
 	return render(request, 'index.html', {
 		'pics': pics,
 		'settings': settings,
