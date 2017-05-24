879d4dce8346ac5138299126a025ba7875501f6a
idear <hui.wu@8lab.cn>
2017-05-24 15:47:11 +0800

    2017-5-24

--- Anniversary/gallery/views.py
+++ Anniversary/gallery/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.conf import settings
 from gallery.models import pic
+from django.utils.timezone import get_current_timezone, utc, get_default_timezone
+import datetime
+
 # Create your views here.
 def index(request):
 	pics = pic.objects.all().order_by("pic_time")
@@ -11,6 +14,10 @@ def index(request):
 		})
 
 def letter(request):
-	return render(request, 'letter.html', {
-		'settings': settings,
-		})
+	n = datetime.datetime.now()
+	pics = pic.objects.create(pic_title="1",pic_time=n.strftime('%Y-%m-%d %H:%M:%S'),pic_path="/",pic_text="a")
+	#pt = pics.pic_time.astimezone(get_default_timezone())
+	# get_default_timezone().normalize()
+	# dt = datetime.datetime.strftime(pt, '%Y-%m-%d %H:%M:%S')
+	
+	return HttpResponse(pics.pic_time)
