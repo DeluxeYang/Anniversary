d106eea4ce75818ea533f102495b2620c22f5605
DeluxeYang <421826500@qq.com>
2017-05-26 00:18:16 +0800

    2017-5-26

--- Anniversary/gallery/admin.py
+++ Anniversary/gallery/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from gallery.models import pic
+from gallery.models import pic, letter
 # Register your models here.
 admin.site.register(pic)
+admin.site.register(letter)
